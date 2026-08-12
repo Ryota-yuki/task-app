4d8af7b6dd4f38141c0a50280b2743497458f793
Ryota-yuki <raven.chain@icloud.com>
2026-08-12 09:34:13 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <h1>タスク管理</h1>
+    <p>タスクアプリを作っています。</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.app {
+  max-width: 600px;
+  margin: 40px auto;
+  padding: 24px;
+  font-family: sans-serif;
+}
+</style>
